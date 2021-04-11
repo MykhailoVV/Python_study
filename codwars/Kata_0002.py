939f559508d423f5b053bef32f48a5dd511de3ba
MykhailoVV <vereshchaka.m@gmail.com>
2021-04-11 22:35:27 +0300

    Add second and third kata

---
+++ codwars/Kata_0002.py
@@ -0,0 +1,12 @@
+'''
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before,
+or after the addition.
+
+The binary number returned should be a string.
+'''
+def add_binary(a,b):
+    x = bin(a + b)
+    return str(x[2:len(x)])
+
+a, b = int(input()), int(input())
+print(add_binary(a, b))
